f1e612d76ea49d1fbe4c775d59b83670af22094c
Anton Palgunov <toxblh@gmail.com>
2020-10-08 22:29:59 +0100

    upd

--- lib.js
+++ lib.js
@@ -3,7 +3,11 @@ const pluginStealth = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(pluginStealth())
 
 async function getNoddleScore({ login, pass }) {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']})
+  let userReport
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+  })
   const page = await browser.newPage()
   await page.goto('https://www.creditkarma.co.uk/account/sign-in', {
     waitUntil: 'networkidle2',
@@ -12,38 +16,36 @@ async function getNoddleScore({ login, pass }) {
     width: 1920,
     height: 1080,
   })
-  await page.waitFor(1000)
-  await page.evaluate(
-    (eLogin, ePass) => {
-      document.querySelector('input[type=text]').value = eLogin
-      document.querySelector('input[type=password]').value = ePass
-      document.querySelector('input[type=submit]').click()
-    },
-    login,
-    pass,
-  )
-  await page.waitForNavigation()
 
-  const updated_date_element = await page.$('.account-summary-header__updated-date')
-  const updated_date = await page.evaluate(
-    element => element.textContent,
-    updated_date_element,
-  )
-  const element = await page.$('.credit-score')
-  const creditScore = await page.evaluate(element => element.textContent, element)
-
-  await page.goto('https://www.creditkarma.co.uk/credit-report', {
-    waitUntil: 'networkidle2',
+  page.on('response', (response) => {
+    if (response.url() === 'https://www.creditkarma.co.uk/graphql') {
+      response.json().then((report) => {
+        if (report !== undefined) {
+          if (report.data !== undefined) {
+            if (report.data.creditReports !== undefined) {
+              if (report.data.creditReports.scoreHistory !== undefined) {
+                userReport = report
+              }
+            }
+          }
+        }
+      })
+    }
   })
 
-  let xpathDate = await page.$x('//*[@id="cr-expires-in-next-days-text"]/strong')
-  let xpathTextContent = await xpathDate[0].getProperty('textContent')
-  let daysToNew = await xpathTextContent.jsonValue()
+  await page.waitForTimeout(1000)
+
+  await page.click('#modal-root > div > div > div:nth-child(2) > div > button')
+  await page.type('input[type=text]', login)
+  await page.type('input[type=password]', pass)
+  await page.click('button[type=submit]')
+
+  await page.waitForTimeout(3000)
 
   const output = {
-    updated_date: daysToNew,
-    report_date: updated_date.slice(9),
-    score: creditScore,
+    updated_date: 'every week',
+    report_date: userReport.data.creditReports.scoreHistory.scores[0].generatedDateTime,
+    score: userReport.data.creditReports.scoreHistory.scores[0].score,
   }
 
   await browser.close()
